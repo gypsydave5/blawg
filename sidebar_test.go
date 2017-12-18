26323ab5d4feff7c57d2459a758337a65cdb266e
David Wickes <dave.wickes@gmail.com>
2017-12-18 12:41:16 +0000

    Better(ish) sidebar test
    Maybe list of links test?

--- sidebar_test.go
+++ sidebar_test.go
@@ -1,7 +1,31 @@
 package blawg
 
-import "testing"
+import (
+	"testing"
+	"strings"
+	"fmt"
+)
 
 func TestSidebar(t *testing.T) {
-	posts := []Post{testPostOne(), testPostTwo()}
+	postOne := testPostOne()
+	postTwo := testPostTwo()
+	posts := []Post{postOne, postTwo}
+
+	var sidebar string = newSidebar(posts)
+
+	htmlLinkFormatString := `<a href="%s">%s</a>`
+	postOneLink := fmt.Sprintf(htmlLinkFormatString, "posts/" + postOne.Path(), postOne.Title)
+	postTwoLink := fmt.Sprintf(htmlLinkFormatString, "posts/" + postTwo.Path(), postTwo.Title)
+
+	if !strings.Contains(sidebar, postOneLink) {
+		t.Errorf(`Expected to find "%s" in "%s"`, postOneLink, sidebar)
+	}
+
+	if !strings.Contains(sidebar, postTwoLink) {
+		t.Errorf(`Expected to find "%s" in "%s"`, postOneLink, sidebar)
+	}
+}
+
+func newSidebar(posts []Post) string {
+	return ""
 }
