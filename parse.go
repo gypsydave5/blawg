857702faeaac5f7148fce9e72a607f7b6389a939
David Wickes <david.wickes@springernature.com>
2024-04-26 10:45:18 +0100

    autogenerate heading ids

--- parse.go
+++ parse.go
@@ -20,7 +20,7 @@ import (
 const dateFormat = "2006-01-02 15:04:05"
 
 var markdownExtensions = blackfriday.WithExtensions(
-	blackfriday.Footnotes | blackfriday.CommonExtensions,
+	blackfriday.Footnotes | blackfriday.CommonExtensions | blackfriday.AutoHeadingIDs,
 )
 
 func parse(rawPost io.Reader) (*Post, error) {
